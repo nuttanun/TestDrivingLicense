17e2831b14a496712e65fdd61d730fd1945e3f8a
Toey <sinunta_35@hotmail.com>
2016-07-20 14:56:49 +0700

    Check Space

--- app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/MainActivity.java
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/MainActivity.java
@@ -4,15 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initail widget
+        userEditText = (EditText) findViewById(R.id.editText6);
+        passwordEditText = (EditText) findViewById(R.id.editText7);
+
+
     } // main Method
 
+    public void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check space
+
+        if (userString.equals("")|| passwordString.equals("")) {
+            //Have space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"Have Space","กรุณากรอกให้ครบทุกช่องค่ะ");
+
+        } else {
+            //no space
+
+        }  //if
+
+
+    }  //  clickSignin
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
