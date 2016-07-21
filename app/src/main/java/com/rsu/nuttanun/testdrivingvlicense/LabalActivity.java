7fe21a391f88b3b985e5ba4b2440eae3d3f9de3f
Toey <sinunta_35@hotmail.com>
2016-07-21 09:57:54 +0700

    Service Activity Sucess

---
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/LabalActivity.java
@@ -0,0 +1,13 @@
+package com.rsu.nuttanun.testdrivingvlicense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LabalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labal);
+    }
+}
--- app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/ServiceActivity.java
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/ServiceActivity.java
@@ -1,13 +1,21 @@
 package com.rsu.nuttanun.testdrivingvlicense;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ServiceActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class ServiceActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private String[] loginStrings;
+    private TextView textView;
+    private ImageView hub1ImageView, hub2ImageView, hub3ImageView, hub4ImageView;
 
 
     @Override
@@ -20,7 +28,52 @@ public class ServiceActivity extends AppCompatActivity {
         loginStrings = getIntent().getStringArrayExtra("login");
         Log.d("20JulyV3", "name Login ==>" + loginStrings[1]);
 
+        //Initial Widget
+
+        textView = (TextView) findViewById(R.id.textView8);
+        hub1ImageView = (ImageView) findViewById(R.id.imageView2);
+        hub2ImageView = (ImageView) findViewById(R.id.imageView3);
+        hub3ImageView = (ImageView) findViewById(R.id.imageView4);
+        hub4ImageView = (ImageView) findViewById(R.id.imageView5);
+
+
+        //Showname
+
+        textView.setText("Welcome" + loginStrings[1]);
+
+        //Image controller
+
+        hub1ImageView.setOnClickListener(this);
+        hub2ImageView.setOnClickListener(this);
+        hub3ImageView.setOnClickListener(this);
+        hub4ImageView.setOnClickListener(this);
 
 
     }   //main Method
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.imageView2:
+                startActivity(new Intent(ServiceActivity.this, CoauseActivity.class));
+                break;
+            case R.id.imageView3:
+                startActivity(new Intent(ServiceActivity.this, LabalActivity.class));
+                break;
+            case R.id.imageView4:
+                startActivity(new Intent(ServiceActivity.this, IshiharaActivity.class));
+                break;
+            case R.id.imageView5:
+                Intent intent = new Intent(ServiceActivity.this, TestActivity.class);
+                intent.putExtra("Login", loginStrings);
+                startActivity(intent);
+                finish();
+
+                break;
+
+        }   // swithc
+
+    }   //on click
 }   // Main class
