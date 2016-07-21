8e48c18e79f5d73ab7cd65162d24d0a31fba5307
Toey <sinunta_35@hotmail.com>
2016-07-21 11:48:43 +0700

    Coaurse List Vive Sucess

--- app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/CoauseActivity.java
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/CoauseActivity.java
@@ -1,16 +1,21 @@
 package com.rsu.nuttanun.testdrivingvlicense;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class CoauseActivity extends AppCompatActivity {
 
@@ -37,6 +42,7 @@ public class CoauseActivity extends AppCompatActivity {
         private Context context;
         private ListView coaurseListView;
         private static final String urlJSON = "http://swiftcodingthai.com/toey/get_coaurse_toey.php";
+        private String[] titleStrings, detailStrings, imageStrings;
 
         public SynCoaurse(Context context,
                           ListView coaurseListView) {
@@ -69,7 +75,50 @@ public class CoauseActivity extends AppCompatActivity {
             super.onPostExecute(s);
 
             Log.d("21JulyV1", "JSON ==> " + s);
-        }
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                titleStrings = new String[jsonArray.length()];
+                detailStrings = new String[jsonArray.length()];
+                imageStrings = new String[jsonArray.length()];
+
+                for (int i=0;i<jsonArray.length();i+=1) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    titleStrings[i] = jsonObject.getString("Title");
+                    detailStrings[i] = jsonObject.getString("Detail");
+                    imageStrings[i] = jsonObject.getString("Image");
+
+                }   //For
+
+                CoaurseAdapter coaurseAdapter = new CoaurseAdapter(context,
+                        titleStrings, detailStrings);
+                coaurseListView.setAdapter(coaurseAdapter);
+
+                coaurseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        Intent intent = new Intent(CoauseActivity.this, DetailCoaurseActivity.class);
+                        intent.putExtra("Title", titleStrings[position]);
+                        intent.putExtra("Detail", detailStrings[position]);
+                        intent.putExtra("Image", imageStrings[position]);
+                        startActivity(intent);
+
+
+
+                    }   // onitem click
+                });
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }   // on post
     }   //SynCoaurse class
 
 
---
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/DetailCoaurseActivity.java
@@ -0,0 +1,13 @@
+package com.rsu.nuttanun.testdrivingvlicense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailCoaurseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_coaurse);
+    }
+}
