16393ca04f90f948995bb98c0dac5f4af3ee3df3
Toey <sinunta_35@hotmail.com>
2016-08-10 13:11:12 +0700

    Get Curret Time

--- app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/ConfirmScoreActivity.java
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/ConfirmScoreActivity.java
@@ -2,8 +2,12 @@ package com.rsu.nuttanun.testdrivingvlicense;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ConfirmScoreActivity extends AppCompatActivity {
 
@@ -11,7 +15,7 @@ public class ConfirmScoreActivity extends AppCompatActivity {
     ///explicit
 
     private String[] loginStrings;
-    private String scoreString;
+    private String scoreString, dateString;
     private TextView nameTextView, dateTextView, scoreTextView;
 
     @Override
@@ -31,9 +35,16 @@ public class ConfirmScoreActivity extends AppCompatActivity {
         loginStrings = getIntent().getStringArrayExtra("login");
         scoreString = getIntent().getStringExtra("Score");
 
+        // Get Current time
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        dateString = dateFormat.format(calendar.getTime());
+
         // Show View
         nameTextView.setText(loginStrings[1] + " " + loginStrings[2]);
         scoreTextView.setText("คะแนนของคุณ = " + scoreString);
+        dateTextView.setText(dateString);
 
 
     } // main method
