1a28e7fa161c9100b2afd89eb874db4280c303da
Toey <sinunta_35@hotmail.com>
2016-08-10 13:42:57 +0700

    update Score to server !!

--- app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/ConfirmScoreActivity.java
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/ConfirmScoreActivity.java
@@ -1,11 +1,21 @@
 package com.rsu.nuttanun.testdrivingvlicense;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.TextView;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -51,7 +61,36 @@ public class ConfirmScoreActivity extends AppCompatActivity {
 
     public void clickOKConfirm(View view) {
 
-    }
+        String urlPHP = "http://swiftcodingthai.com/toey/add_score.php";
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("id_login", loginStrings[0])
+                .add("Date", dateString)
+                .add("Score", scoreString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+                Intent intent = new Intent(ConfirmScoreActivity.this, ScoreListView.class);
+                intent.putExtra("login", loginStrings);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    } // Click ok
 
 
 
---
+++ app/src/main/java/com/rsu/nuttanun/testdrivingvlicense/ScoreListView.java
@@ -0,0 +1,13 @@
+package com.rsu.nuttanun.testdrivingvlicense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_list_view);
+    }
+}
